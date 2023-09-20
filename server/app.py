01aac9cd32a34181d719abef3da1442acaf35d18
MutaiAllan <MutaiAllan>
2023-09-21 00:51:44 +0300

    Added all the views

--- server/app.py
+++ server/app.py
@@ -14,13 +14,51 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=["GET", "POST"])
 def messages():
-    return ''
+    if request.method == "GET":
+        messages = []
+        for message in Message.query.order_by(Message.created_at).all():
+            message_dict = message.to_dict()
+            messages.append(message_dict)
+        response = make_response(jsonify(messages), 200)
+        return response
+    elif request.method == "POST":
+        json_data = request.json
+        new_message = Message(
+            body=json_data.get("body"),
+            username=json_data.get("username")
+        )
+        db.session.add(new_message)
+        db.session.commit()
+        message_dict = new_message.to_dict()
+        response = make_response(jsonify(message_dict), 201)
+        return response
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods = ["PATCH", "DELETE"])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+    if request.method == "PATCH":
+        message = Message.query.filter_by(id=id).first()
+        json_data = request.json
+        for key in json_data:
+            setattr(message, key, json_data.get(key))
+
+        #db.session.add(message)
+        db.session.commit()
+        message_dict = message.to_dict()
+        response = make_response(jsonify(message_dict), 200)
+        return response
+    
+    elif request.method == "DELETE":
+        db.session.delete(message)
+        db.session.commit()
+        response_body = {
+            "delete_successful": True,
+            "message": "Message seleted!"
+        }
+        response = make_response(jsonify(response_body), 200)
+        return response
 
 if __name__ == '__main__':
     app.run(port=5555)
